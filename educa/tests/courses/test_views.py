4f01f67440a761c312a656f217f2a3052961dcce
Jayachitra <jayachitra@testpress.in>
2022-12-09 18:06:31 +0530

    Create content management system in courses app

---
+++ educa/tests/courses/test_views.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.urls import reverse
+from tests.courses.test_model_mixin import Modelmixin
+from courses.models import Subject, Course, Module
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import Permission, User
+
+
+class TestListView(Modelmixin, TestCase):
+    def test_course_list_tamplate_used(self):
+        permission = Permission.objects.get(name="Can view course")
+        self.user.user_permissions.add(permission)
+        self.client.login(username="jai", password="123")
+        response = self.client.get((reverse("course:manage_course_list")))
+        self.assertTemplateUsed(response, "courses/manage/course/list.html")
+
+    def test_course_DeleteView_tamplate_used(self):
+        permission = Permission.objects.get(name="Can delete course")
+        self.user.user_permissions.add(permission)
+        self.client.login(username="jai", password="123")
+        response = self.client.get(
+            (reverse("course:delete", args=[self.user.id]))
+        )
+        self.assertTemplateUsed(response, "courses/manage/course/delete.html")
