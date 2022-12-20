bb3ac16d953aec8fb0e178a3c29adbade78503c2
Jayachitra <jayachitra@testpress.in>
2022-12-20 12:07:39 +0530

    Create Detail View For Course

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -12,6 +12,7 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from django.db.models import Count
+from django.views.generic.detail import DetailView
 
 
 class OwnerMixin(object):
@@ -166,3 +167,8 @@ class CourseListView(TemplateResponseMixin, View):
         return self.render_to_response(
             {"subjects": subjects, "courses": courses, "subject": subject}
         )
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
